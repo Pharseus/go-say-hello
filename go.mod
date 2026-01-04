5136a6ed2e71d0b7024e28dcc7c8439543f1088f
alderyeon <23k10057@student.unika.ac.id>
2026-01-04 20:17:11 +0700

    update say hello with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,4 @@
-module github.com/Pharseus/go-say-hello
+// module github.com/Pharseus/go-say-hello/
+module github.com/Pharseus/go-say-hello/v2
 
 go 1.25.5
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,11 @@
 package gosayhello
 
-func SayHello() string {
-	return "Hello World"
+// func SayHello() string {
+// 	return "Hello World"
+// }
+
+// major change
+// karena merusak kode sebelunya yang awlanya tidak ada parameter
+func SayHello(name string) string {
+	return "Hello World" + name
 }
